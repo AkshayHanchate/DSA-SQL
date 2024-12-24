0f8ee4e42b4f7df1349e5e703bf5edd5a8fcae0d
Akshay Hanchate <158104333+AkshayHanchate@users.noreply.github.com>
2024-12-24 11:56:06 -0500

    Add files via upload

--- Contains_Duplicate II.py
+++ Contains_Duplicate II.py
@@ -1,11 +1,28 @@
+# class Solution:
+#     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+#         seen = {}
+#
+#         for i, val in enumerate(nums):
+#             if val in seen and i - seen[val] <= k:
+#                 return True
+#             else:
+#                 seen[val] = i
+#
+#         return False
+
+#Sliding Window Method
+
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        seen = {}
+        window = set()
+        L = 0
 
-        for i, val in enumerate(nums):
-            if val in seen and i - seen[val] <= k:
+        for r in range(len(nums)):
+            if r - L > k:
+                window.remove(nums[L])
+                L += 1
+            if nums[r] in window:
                 return True
             else:
-                seen[val] = i
-
+                window.add(nums[r])
         return False
