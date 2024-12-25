e43bb91955268e03236577cadfc714fb33f5aae8
Akshay Hanchate <158104333+AkshayHanchate@users.noreply.github.com>
2024-12-25 11:38:48 -0500

    Add files via upload

---
+++ Department_Top_Three_Salaries.sql
@@ -0,0 +1,8 @@
+
+select d.name as department, e.name as employee, e.salary as Salary
+from Employee e
+join Department d
+on e.Departmentid = d.id
+where 3 > (select count(distinct e2.salary)
+from Employee e2 where
+e2.salary > e.salary and e.DepartmentId = e2.DepartmentId)
