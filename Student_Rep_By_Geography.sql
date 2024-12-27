711c156d1cf475b41900382f87fdb3103a2c489f
Akshay Hanchate <158104333+AkshayHanchate@users.noreply.github.com>
2024-12-26 21:40:04 -0500

    Add files via upload

---
+++ Student_Rep_By_Geography.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+with cte as (
+    select *, row_number() over (partition by continent order by name) as rn
+    from student
+)
+
+select
+max(case when continent = 'America' then name end) as America,
+max(case when continent = 'Asia' then name end) as Asia,
+max(case when continent = 'Europe' then name end) as Europe
+from cte
+group by rn
