02cb3710d9cf2f795e9722561d4542981f5a2951
Akshay Hanchate <158104333+AkshayHanchate@users.noreply.github.com>
2025-01-21 12:05:58 -0500

    Add files via upload

---
+++ Premier_League_Table_Ranking_III.sql
@@ -0,0 +1,14 @@
+
+WITH cte1 AS (
+SELECT
+    season_id,
+    team_id,
+    team_name,
+    (wins*3+draws*1+losses*0) AS points,
+    (goals_for - goals_against) AS goal_difference
+FROM SeasonStats
+)
+SELECT
+    *,
+    DENSE_RANK()OVER(PARTITION BY season_id ORDER BY points DESC, goal_difference DESC, team_name) AS `position`
+FROM cte1
